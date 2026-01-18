32022b9ce77bc869b931bbd79d01b62c898addb3
Alexey <akakindinov@gmail.com>
2026-01-18 19:22:20 +0300

    Максимально сырой геймплей + тз

--- main.py
+++ main.py
@@ -528,6 +528,7 @@ class Player(arcade.Sprite):
     def update(self):
         if not self.is_alive:
             return
+
         super().update()
         if self.physics_engine:
             self.can_jump = self.physics_engine.can_jump()
